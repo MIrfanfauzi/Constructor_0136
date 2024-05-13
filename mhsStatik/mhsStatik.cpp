922461ad85ca0315c3a64084df6a06bf54098606
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-13 10:45:32 +0700

    membuat prosedur printAll

--- mhsStatik/mhsStatik.cpp
+++ mhsStatik/mhsStatik.cpp
@@ -27,12 +27,18 @@ void mahasiswa::setID()
     id = ++nim;
 }
 
-
+void mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
 
 
 int main()
 {
     
+
 }
 
 
