f708039d1a4637fc29c074914d71b131a7e0f592
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-13 10:33:01 +0700

    membuat class mahasiswa

--- mhsStatik/mhsStatik.cpp
+++ mhsStatik/mhsStatik.cpp
@@ -3,7 +3,22 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
 
+    void setID();
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
+
+};
 
 int main()
 {
