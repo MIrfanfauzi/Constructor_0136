db04fc0b43904df1454963b9e256246f9b64ecfe
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-13 11:17:33 +0700

    membuat int main

--- statikfunction/statikfunction.cpp
+++ statikfunction/statikfunction.cpp
@@ -18,7 +18,7 @@ public:
     mahasiswa(string pnama) : nama(pnama) { setID(); }
 };
 
-long long int mahasiswa::nim = 0;
+long long int mahasiswa::nim = 20230140000;
 
 void mahasiswa::setID()
 {
@@ -34,7 +34,19 @@ void mahasiswa::printAll()
 
 int main()
 {
-    
+    mahasiswa mhs1("Irfan Fauzi");
+    mahasiswa mhs2("Ridho Rawr");
+    mahasiswa::setNim(2023014020);
+    mahasiswa mhs3("Erin Dito ");
+    mahasiswa mhs4("Eko Hidayat ");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
+    return 0;
 }
 
 
