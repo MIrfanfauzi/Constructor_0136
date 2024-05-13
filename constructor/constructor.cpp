6f9c1c280ed011f1f08ee1eba4915110efd7c589
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-13 13:08:30 +0700

    membuat int main

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -17,12 +17,18 @@ class Mahasiswa
         Mahasiswa(int iNim)
         {
             nim = iNim;
-        }
+            nim = 0;
+        };
         Mahasiswa(string iNama)
         {
             nama = iNama;
-        }
-        Mahasiswa(int iNim, string iNama);
+        };
+        Mahasiswa(int iNim, string iNama)
+        {
+            nim = iNim;
+            nama = iNama;
+            
+        };
        
         void cetak()
         {
@@ -34,12 +40,19 @@ class Mahasiswa
 
 
 
-
-
-
 int main()
 {
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("Erin Dito ");
+    Mahasiswa mhs4(30, "Eko Hidayat ");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
     
+    return 0;
 }
 
 
